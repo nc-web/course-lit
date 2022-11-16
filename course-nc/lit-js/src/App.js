642c2bc9016149be4be3b3af255f43bf31e1fc45
Andres Giraldo Arenas <andresganc@gmail.com>
2022-11-15 23:05:27 -0500

    New: Lit JS Hello World

---
+++ course-nc/lit-js/src/App.js
@@ -0,0 +1,11 @@
+
+// import { HelloWorld } from './01-basic/hello-world/HelloWorld.js'
+
+export function App() {
+
+    $root = document.getElementById('root').innerHTML = `<h2>APP</h2>`
+
+    // $root.appendChild(HelloWorld())
+
+
+}
