08551405bea2b09712be3849a06c56dda94f218e
Andres Giraldo Arenas <andresganc@gmail.com>
2022-11-18 12:05:17 -0500

    Mod: Exercises Calculator

--- exercises/exercises-calculator-js/src/views/calculator01/Calculator01.js
+++ exercises/exercises-calculator-js/src/views/calculator01/Calculator01.js
@@ -3,58 +3,89 @@ import { html, css, LitElement } from 'lit'
 
 export class Calculator01 extends LitElement {
   static properties = {
-    state01: { type: Number }
+    stateCurrent: { type: Number },
+    stateDisplayTop: { type: Number },
+    stateDisplayBottom: { type: Number }
   }
 
   constructor () {
     super()
-    this.state01 = 0
+    this.stateCurrent = 0
+    this.stateDisplayTop = 0
+    this.stateDisplayBottom = 0
   }
 
   render () {
     return html`
       <article class="calc">
         <div class="calc__display">
-          <div class="calc__display-top">
-            <h1>0</h1>
-          </div>
-          <div class="calc__display-bottom">
-            <h4>0</h4>
-          </div>
+            <h1 class="calc__display-top">${this.stateDisplayTop}</h1>
+            <h4 class="calc__display-bottom">${this.stateDisplayBottom}</h4>
         </div>
 
         <div class="calc__b">
-          <button name='' class="calc__b-button-ac">AC</button>
-          <button name='' class="calc__b-button-s">+/-</button>
-          <button name='' class="calc__b-button-s">%</button>
-          <button name='' class="calc__b-button-s">/</button>
+          <button name='AC' class="calc__b-button-ac" @click=${this.handleClear}>AC</button>
+          <button name='Back' class="calc__b-button-back" @click=${this.handleBack}>
+            <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M22,3H7C6.31,3 5.77,3.35 5.41,3.88L0,12L5.41,20.11C5.77,20.64 6.31,21 7,21H22A2,2 0 0,0 24,19V5A2,2 0 0,0 22,3M19,15.59L17.59,17L14,13.41L10.41,17L9,15.59L12.59,12L9,8.41L10.41,7L14,10.59L17.59,7L19,8.41L15.41,12" />
+            </svg>
+          </button>
+          <button name='%' class="calc__b-button-s" @click=${this.handleButton}>%</button>
+          <button name='/' class="calc__b-button-s" @click=${this.handleButton}>/</button>
 
-          <button name='' class="calc__b-button-num">7</button>
-          <button name='' class="calc__b-button-num">8</button>
-          <button name='' class="calc__b-button-num">9</button>
-          <button name='' class="calc__b-button-s">x</button>
+          <button name='7' class="calc__b-button-num" @click=${this.handleButton}>7</button>
+          <button name='8' class="calc__b-button-num" @click=${this.handleButton}>8</button>
+          <button name='9' class="calc__b-button-num" @click=${this.handleButton}>9</button>
+          <button name='*' class="calc__b-button-s" @click=${this.handleButton}>x</button>
 
-          <button name='' class="calc__b-button-num">4</button>
-          <button name='' class="calc__b-button-num">5</button>
-          <button name='' class="calc__b-button-num">6</button>
-          <button name='' class="calc__b-button-s">-</button>
+          <button name='4' class="calc__b-button-num" @click=${this.handleButton}>4</button>
+          <button name='5' class="calc__b-button-num" @click=${this.handleButton}>5</button>
+          <button name='6' class="calc__b-button-num" @click=${this.handleButton}>6</button>
+          <button name='-' class="calc__b-button-s" @click=${this.handleButton}>-</button>
 
-          <button name='' class="calc__b-button-num">1</button>
-          <button name='' class="calc__b-button-num">2</button>
-          <button name='' class="calc__b-button-num">3</button>
-          <button name='' class="calc__b-button-s">+</button>
+          <button name='1' class="calc__b-button-num" @click=${this.handleButton} >1</button>
+          <button name='2' class="calc__b-button-num" @click=${this.handleButton}>2</button>
+          <button name='3' class="calc__b-button-num" @click=${this.handleButton}>3</button>
+          <button name='+' class="calc__b-button-s" @click=${this.handleButton} >+</button>
         </div>
         <div class="calc__b2">
           <div>
-            <button name='' class="calc__b-button-num">0</button>
-            <button name='' class="calc__b-button-num">.</button>
+            <button name='0' class="calc__b-button-num" @click=${this.handleButton}>0</button>
+            <button name='.' class="calc__b-button-num" @click=${this.handleButton}>.</button>
           </div>
           <div>
-            <button name='' class="calc__b-button-equal">=</button>
+            <button name='' class="calc__b-button-equal" @click=${this.handleEqual}>=</button>
           </div>
         </div>
       </article>
     `
+  }
+
+  handleButton (e) {
+    e.preventDefault()
+    this.stateCurrent += e.target.name
+    this.stateDisplayBottom = this.stateCurrent
+  }
+
+  handleClear (e) {
+    e.preventDefault()
+    this.stateCurrent = 0
+    this.stateDisplayTop = 0
+    this.stateDisplayBottom = 0
+  }
+
+  handleBack (e) {
+    e.preventDefault()
+    this.stateCurrent = this.stateCurrent.slice(0, this.stateCurrent.length - 1)
+    this.stateDisplayBottom = this.stateCurrent
+  }
+
+  handleEqual (e) {
+    e.preventDefault()
+    const temp = eval(this.stateCurrent.toString())
+    console.log('Valor: ', temp)
+    // this.stateDisplayTop = Function(temp)
+    // this.stateCurrent = 0
   }
 
   static styles = css`
@@ -74,20 +105,20 @@ export class Calculator01 extends LitElement {
         .calc__display {
           width: 17.5rem;
           padding: .1rem 1rem;
-          background-color: #9E9E9E;
+          background-color: #757575;
           margin: 1rem .5rem 2rem .5rem;
           border-radius: .5rem;
         }
 
         .calc__display-top {
-          font-size: 36px;
+          font-size: 45px;
           text-align: right;
           color: #F5F5F5;
         }
 
         .calc__display-bottom {
           text-align: right;
-          font-size: 24px;
+          font-size: 20px;
           color: #FFB74D;
         }
 
@@ -107,6 +138,7 @@ export class Calculator01 extends LitElement {
         }
 
         .calc__b-button-ac {
+          
           width: 3.5rem;
           height: 3.5rem;
           font-size: 20px;
@@ -119,6 +151,24 @@ export class Calculator01 extends LitElement {
         }
 
         .calc__b-button-ac:hover {
+          background-color: #757575;
+        }
+
+        .calc__b-button-back {
+          padding-top: .3rem;
+          padding-left: .2rem;
+          width: 3.5rem;
+          height: 3.5rem;
+          font-size: 20px;
+          cursor: pointer;
+          border-radius: 50%;
+          border: none;
+          color: #F57C00;
+          background-color: #EEEEEE;
+          transition: .2s;
+        }
+
+        .calc__b-button-back:hover {
           background-color: #757575;
         }
 
