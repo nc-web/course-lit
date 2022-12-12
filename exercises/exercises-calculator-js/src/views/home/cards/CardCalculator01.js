24a7a66cf38abe5a4c1754255ccef7c310686f9e
Andres Giraldo <andresganc@gmail.com>
2022-12-12 10:37:08 -0500

    New: Exercises Section JS

--- exercises/exercises-calculator-js/src/views/home/cards/CardCalculator01.js
+++ exercises/exercises-calculator-js/src/views/home/cards/CardCalculator01.js
@@ -1,16 +1,7 @@
 
-import { html, css, LitElement } from 'lit'
+import { LitElement, html, css } from 'lit'
 
 export class CardCalculator01 extends LitElement {
-  static properties = {
-    state01: { type: Number }
-  }
-
-  constructor () {
-    super()
-    this.state01 = 0
-  }
-
   render () {
     return html`
             <a href='../../../../pages/calculator01.html' >
@@ -18,6 +9,9 @@ export class CardCalculator01 extends LitElement {
                     <div class='card__h'>
                         <svg style="width:60px; height:60px" viewBox="0 0 24 24">
                             <path fill="#1E88E5" d="M7,2H17A2,2 0 0,1 19,4V20A2,2 0 0,1 17,22H7A2,2 0 0,1 5,20V4A2,2 0 0,1 7,2M7,4V8H17V4H7M7,10V12H9V10H7M11,10V12H13V10H11M15,10V12H17V10H15M7,14V16H9V14H7M11,14V16H13V14H11M15,14V16H17V14H15M7,18V20H9V18H7M11,18V20H13V18H11M15,18V20H17V18H15Z" />
+                        </svg>
+                        <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                            <path fill="" d="M6,17C6,15 10,13.9 12,13.9C14,13.9 18,15 18,17V18H6M15,9A3,3 0 0,1 12,12A3,3 0 0,1 9,9A3,3 0 0,1 12,6A3,3 0 0,1 15,9M3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3H5C3.89,3 3,3.9 3,5Z" />
                         </svg>
                     </div>
                     <div class='card__b'>
@@ -45,8 +39,7 @@ export class CardCalculator01 extends LitElement {
             border-radius: .5rem;
             box-shadow: 1px 2px 1px 1px rgba(0, 0, 0, 0.1);
             cursor: pointer;
-            transition: .3s;
-            
+            transition: .3s; 
         }  
 
         .card:hover {
