691b813e427c429c739b28c5795510b1bbd9f168
Andres Giraldo <andresganc@gmail.com>
2023-01-13 18:05:26 -0500

    Mod: My Page Andres JS

--- exercises/my-page-andres-js/src/App.js
+++ exercises/my-page-andres-js/src/App.js
@@ -3,12 +3,14 @@ import { LitElement, html, css } from 'lit'
 
 // Components
 import './views/nav/index.js'
+import './views/header/index.js'
 
 export class App extends LitElement {
 
     render() {
         return html`
             <index-nav></index-nav>
+            <index-header></index-header>
         `
     }
 
