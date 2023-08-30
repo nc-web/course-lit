3116dcceb3593286c1493c16b5cbe7d2b5b18bad
Andres Giraldo <andresganc@gmail.com>
2023-08-30 12:31:25 -0500

    Mod: Todo List - Lit Doc TS

--- examples/todo-list-lit-doc-ts/src/todo-list-lit-doc-ts.ts
+++ examples/todo-list-lit-doc-ts/src/todo-list-lit-doc-ts.ts
@@ -1,15 +1,41 @@
 import { LitElement, html, css } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
+import { ref, Ref, createRef } from 'lit/directives/ref.js';
 
 @customElement('todo-list-lit-doc-ts')
 export class TodoListLitDocTs extends LitElement {
 
-  @property({ type: String }) header = 'My app';
+  @property({ type: String }) 
+  message = 'Todo List'
+
+  @property({type: String})
+  name = 'Task'
+
+  @property({type: Boolean})
+  checked = false 
+
+  handleClick() {
+    console.log('Handle Click')
+  }
+
+  changeName(event: Event) {
+    const input = event.target as HTMLInputElement
+    this.name = input.value;
+    console.log('Change Name', this.name)
+  }
+
+  inputRef: Ref<HTMLInputElement> = createRef();
 
   render() {
     return html`
       <main>
         <h4>TODO LIST - LIT DOCUMENTATION (TS)</h4>
+        <div>
+          <input type='text' placeholder='Enter your task' @input=${this.changeName} ${ref(this.inputRef)}>
+          <button @click=${this.handleClick} >Add Task</button>
+          <p>Welcome to the Lit tutorial!</p>
+          <p>This is the ${this.message} code.</p>
+        </div>
       </main>
     `;
   }
@@ -20,5 +46,10 @@ export class TodoListLitDocTs extends LitElement {
       margin: 0;
       padding: 0;
     }
-  `;
+
+    main {
+      display: grid;
+      justify-content: center;
+    }
+  `
 }
