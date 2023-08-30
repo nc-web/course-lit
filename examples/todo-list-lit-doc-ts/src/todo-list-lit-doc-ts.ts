90c9449add4172abf18de70bee966cf2d1797b64
Andres Giraldo <andresganc@gmail.com>
2023-08-30 10:54:35 -0500

    New: Examples: TodoList Lit Documentation JS

--- examples/todo-list-lit-doc-ts/src/todo-list-lit-doc-ts.ts
+++ examples/todo-list-lit-doc-ts/src/todo-list-lit-doc-ts.ts
@@ -1,38 +1,16 @@
 import { LitElement, html, css } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
 
-const logo = new URL('../../assets/open-wc-logo.svg', import.meta.url).href;
-
 @customElement('todo-list-lit-doc-ts')
 export class TodoListLitDocTs extends LitElement {
+
   @property({ type: String }) header = 'My app';
 
   render() {
     return html`
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/TodoListLitDocTs.ts</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
+        <h4>TODO LIST - LIT DOCUMENTATION (TS)</h4>
       </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
     `;
   }
 
