bcf5a637397869469ef2dd17d439566283af070e
Andres Giraldo <andresganc@gmail.com>
2024-02-02 10:54:57 -0500

    Mod: Exercises: Todo List Basic Ok

--- exercises/01-basic-exercises/todo-list-js/src/todo-list-basic/TodoListBasic.js
+++ exercises/01-basic-exercises/todo-list-js/src/todo-list-basic/TodoListBasic.js
@@ -3,50 +3,60 @@ import { LitElement, html, css } from 'lit'
 
 export class TodoListBasic extends LitElement {
 
+  // STATES - PROPS - LIFE CYCLE
   static properties = {
-    _counter: {state: true},
-    inpuTask: {type: String}
+    propInputTask: {type: String},
+    propTasks: {state: true}
   }
 
   constructor() {
     super()
-    this.inpuTask = ''
+    this.propInpuTask = ''
+    this.propTasks = []
   }
 
 
+  // VIEWS
   render() {
-    return html `
-            <section>
-                <div>
-                  <h2>TODO LIST BASIC</h2>
+    const items = this.propTasks
+
+    return html`
+            <section class='tlb'>
+
+                <div class='tlb__header'>
+                  <h2 class='tlb__header_title'>TODO LIST BASIC</h2>
                 </div>
                 
-                <form>
-                  <input type='text' @change=${this.onChangeInputTask} placeholder='Add task'>
-                  <button type='button' @click=${this.handleInputTask}>Add Task</button>
+                <form class='tlb__form'>
+                  <input class='tlb__form_inputTask' @change=${this.onChangeInputTask} type='text' placeholder='Add task' value=${this.propInputTask}>
+                  <button class='tlb__form_buttonTask' type='button' @click=${this.handleInputTask}>Add Task</button>
                 </form>
 
-                <div>
-                  <p>${this.inpuTask}</p>
+                <div class='tlb__tasks'>
+                  ${items.map((x) => html`
+                    <p>${x}</p>
+                  `)}
                 </div>
 
             </section>
         `
   }
 
+  // LOGIC
   onChangeInputTask(e) {
-    e.preventDefault()
-    this.inpuTask = e.target.value
-    // this.inpuTask = 'Andres'
-    console.log(this.inputTask)
-    // console.log('Hola')
+    // e.preventDefault()
+    let inputTask = e.target.value
+    this.propInputTask = inputTask
+    console.log(this.propInpuTask)
   }
 
   handleInputTask() {
-    console.log(this.inputTask)
-    // console.log('Hola')
+    this.propTasks.push(this.propInputTask)
+    console.log(this.propInpuTask)
+    this.propInputTask = ''
   }
 
+  // STYLES
   static styles = css`
   
     :host {
@@ -55,8 +65,26 @@ export class TodoListBasic extends LitElement {
         padding: 0;
     }
 
-  `
+    .tlb__form_inputTask {
+      padding: .5rem;
+      border-radius: .4rem;
+    }
 
+    .tlb__form_buttonTask {
+      padding: .5rem 1rem;
+      border-radius: .3rem;
+      color: #FFFFFF;
+      background-color: #0D47A1;
+      cursor: pointer;
+      border-style: none;
+
+      &:hover {
+        transition: .3s;
+        background-color: #424242;
+      }
+    }
+
+  `
 }
 
 customElements.define('todo-list-basic', TodoListBasic)
