7b468665a0837b6ddf57ec186f9a1c88e26b82c5
Andres Giraldo <andresganc@gmail.com>
2024-02-02 19:05:44 -0500

    Mod; Exercises: Todo List Advanced TS

---
+++ exercises/01-basic-exercises/todo-list-ts/src/todo-list-advanced/TodoListAdvanced.ts
@@ -0,0 +1,147 @@
+
+import { LitElement, html, css } from 'lit'
+import { customElement, state, property, query } from 'lit/decorators.js'
+
+@customElement('todo-list-advanced')
+export class TodoListAdvanced extends LitElement {
+
+  // STATE - PROPS - LIFE CYCLE
+  @state()
+  private stateTasks: string[]
+
+  @property({type: String})
+    propInputTask
+
+  @property({type: Boolean})
+    propHideCompleted
+
+  constructor() {
+    super()
+    this.stateTasks = []
+    this.propInputTask = ''
+    this.propHideCompleted = false
+  }
+
+  // VIEWS
+  render() {
+    return html`
+            <section class='tla'>
+                
+                <div class='tla__header'>
+                  <h2 class='tla__header_title'>TODO LIST ADVANCED - TS</h2>
+                </div>
+
+                <form class='tla__form'>
+                  <input id='input_task' @change=${this.onChangeTask} class='tla__form_inputTask' type='text' placeholder='Writer task'/>
+                  <button @click=${this.handleButtonTask} class='tla__form_buttonTask' type='button'>Add task</button>
+                </form>
+
+                <div class='tla__taks'>
+                  ${this.stateTasks.map(x => html`
+                    <div class='tla__task_items'>
+                      <p key=${x} >${x}</p>
+                      <svg @click=${this.handleDeleteTask(x)} class='tla__task_items_svgDelete' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Delete task</title>  <path fill='#D50000' d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>
+                      <svg @click=${this.handleEditTask} class='tla__task_items_svgEdit' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Edit task</title> <path fill='#FFAB00' d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>
+                      <svg @click=${this.handleChekedTask} class='tla__task_items_svgCheked' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Complete task</title><path fill='#64DD17' d="M9,20.42L2.79,14.21L5.62,11.38L9,14.77L18.88,4.88L21.71,7.71L9,20.42Z" /></svg>
+                    </div>  
+                  `)}
+                </div>    
+
+            </section>
+        `
+  }
+
+  // LOGIC
+  @query('#input_task')
+    queryInput!: HTMLInputElement
+
+  onChangeTask() {
+    const inputTask = this.queryInput.value
+    this.propInputTask = inputTask
+    console.log(this.propInputTask)
+    this.requestUpdate()
+  }
+
+  handleButtonTask() {
+    this.stateTasks.push(this.propInputTask)
+    this.queryInput.value = ''
+    console.log(this.stateTasks)
+    this.requestUpdate()
+  }
+
+  handleDeleteTask(x: {}) {
+    // this.stateTasks
+    console.log('Deleted id:', x)
+  }
+
+  handleEditTask() {
+    // this.stateTasks
+    console.log('Edited')
+  }
+
+  handleChekedTask() {
+    // this.stateTasks
+    console.log('Cheked')
+  }
+
+  // STYLES
+  static styles = css`
+    
+    :host {
+        display: inline-block;
+    }
+
+    .tla {
+      display: grid;
+      justify-content: center;
+      align-items: center;
+      padding: 2rem;
+    }
+
+    .tla__form {
+      margin: 2rem 0;
+    }
+
+    .tla__form_inputTask {
+      padding: .5rem;
+      border-radius: .3rem;
+      border-style: none;
+    }
+
+    .tla__form_buttonTask {
+      padding: .5rem 1rem;
+      border-radius: .3rem;
+      color: #FFFFFF;
+      background-color: #0D47A1;
+      border-style: none;
+      cursor: pointer;
+
+      &:hover {
+        transition: .3s;
+        background-color: #616161;
+      }
+    }
+
+    .tla__task_items {
+      display: flex;
+    }
+
+    .tla__task_items_svgDelete {
+      width: 1.5rem;
+      margin: 0 .3rem 0 3rem;
+      cursor: pointer;
+    }
+
+    .tla__task_items_svgEdit {
+      width: 1.5rem;
+      margin: 0 .3rem 0 .3rem;
+      cursor: pointer;
+    }
+
+    .tla__task_items_svgCheked {
+      width: 1.5rem;
+      margin: 0 .3rem 0 .3rem;
+      cursor: pointer;
+    }
+  `
+}
--- exercises/01-basic-exercises/todo-list-ts/src/todo-list-basic/TodoListBasic.ts
+++ exercises/01-basic-exercises/todo-list-ts/src/todo-list-basic/TodoListBasic.ts
@@ -1,33 +1,112 @@
 
 import { LitElement, html, css } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
+import { customElement, state, property, query } from 'lit/decorators.js'
 
 @customElement('todo-list-basic')
 export class TodoListBasic extends LitElement {
 
+  // STATE - PROPS - LIFE CYCLE
+  @state()
+  private stateTasks: string[]
+
+  @property({type: String})
+    propInputTask
+
+  @property({type: Boolean})
+    propHideCompleted
+
+  constructor() {
+    super()
+    this.stateTasks = []
+    this.propInputTask = ''
+    this.propHideCompleted = false
+  }
+
+  // VIEWS
   render() {
     return html`
-            <section>
+            <section class='tla'>
                 
-                <div>
-                    <h2>TODO LIST BASIC - TS</h2>
+                <div class='tla__header'>
+                  <h2 class='tla__header_title'>TODO LIST BASIC - TS</h2>
                 </div>
 
-                <div>
+                <form class='tla__form'>
+                  <input id='input_task' @change=${this.onChangeTask} class='tla__form_inputTask' type='text' placeholder='Writer task'/>
+                  <button @click=${this.handleButtonTask} class='tla__form_buttonTask' type='button'>Add task</button>
+                </form>
 
-                </div>
-
-                <div>
-
+                <div class='tla__taks'>
+                  ${this.stateTasks.map(x => html`
+                    <div class='tla__task_items'>
+                      <p>${x}</p>
+                    </div>  
+                  `)}
                 </div>    
+
             </section>
         `
   }
 
+  // LOGIC
+  @query('#input_task')
+    queryInput!: HTMLInputElement
+
+  onChangeTask() {
+    const inputTask = this.queryInput.value
+    this.propInputTask = inputTask
+    console.log(this.propInputTask)
+    this.requestUpdate()
+  }
+
+  handleButtonTask() {
+    this.stateTasks.push(this.propInputTask)
+    this.queryInput.value = ''
+    console.log(this.stateTasks)
+    this.requestUpdate()
+  }
+
+  // STYLES
   static styles = css`
     
     :host {
         display: inline-block;
     }
+
+    .tla {
+      display: grid;
+      justify-content: center;
+      align-items: center;
+      padding: 2rem;
+    }
+
+    .tla__form {
+      margin: 2rem 0;
+    }
+
+    .tla__form_inputTask {
+      padding: .5rem;
+      border-radius: .3rem;
+      border-style: none;
+    }
+
+    .tla__form_buttonTask {
+      padding: .5rem 1rem;
+      border-radius: .3rem;
+      color: #FFFFFF;
+      background-color: #0D47A1;
+      border-style: none;
+      cursor: pointer;
+
+      &:hover {
+        transition: .3s;
+        background-color: #616161;
+      }
+    }
+
+    .tla__task_items {
+      display: flex;
+    }
+
   `
 }
