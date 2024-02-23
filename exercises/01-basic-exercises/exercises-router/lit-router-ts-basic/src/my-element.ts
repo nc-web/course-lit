8784ec1b450a0c60c398b8adb41316cc73ccb54b
Andres Giraldo <andresganc@gmail.com>
2024-02-23 16:15:11 -0500

    Mod: Exercises Router: Lit Router TS & Vaadin Router TS

--- exercises/01-basic-exercises/exercises-router/lit-router-ts-basic/src/my-element.ts
+++ exercises/01-basic-exercises/exercises-router/lit-router-ts-basic/src/my-element.ts
@@ -37,10 +37,7 @@ export class MyElement extends LitElement {
   static styles = css`
 
     :host {
-      max-width: 1280px;
-      margin: 0 auto;
-      padding: 2rem;
-      text-align: center;
+      display: block;
     }
     
   `
