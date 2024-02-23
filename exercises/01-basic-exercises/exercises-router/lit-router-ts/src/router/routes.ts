327f028de7d109ba7333e96eabea636015695349
Andres Giraldo <andresganc@gmail.com>
2024-02-23 08:29:04 -0500

    New: Exercises: Router:  Structure Lit Router TS

---
+++ exercises/01-basic-exercises/exercises-router/lit-router-ts/src/router/routes.ts
@@ -0,0 +1,43 @@
+
+import { Router } from '@vaadin/router'
+
+import '../my-element'
+
+// Pages
+import '../views/home/HomeDesktop'
+import '../views/about/AboutDesktop'
+import '../views/login/LoginDesktop'
+
+
+const routes = [
+
+    { 
+        path: '/', 
+        component: 'home-desktop',
+        children: [
+            
+        ]
+        
+    },
+
+    { 
+        path: 'about', 
+        component: 'about-desktop',
+        action: async () => {
+            await import('../views/about/AboutDesktop')
+        }
+    },
+
+    { 
+        path: 'login', 
+        component: 'login-desktop',
+        action: async () => {
+            await import('../views/login/LoginDesktop')
+        }
+    },
+
+]
+
+const outlet = document.getElementById('outlet');
+export const router = new Router(outlet);
+router.setRoutes(routes);
