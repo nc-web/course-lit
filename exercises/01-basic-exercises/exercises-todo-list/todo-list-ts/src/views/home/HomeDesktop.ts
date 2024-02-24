785cc499e0dbb4586fa71cd9937bea2e1f92dfe9
Andres Giraldo <andresganc@gmail.com>
2024-02-24 18:31:02 -0500

    Mod: Exercises Todo List: Main Layout Responsive, Menu Main Mobile

--- exercises/01-basic-exercises/exercises-todo-list/todo-list-ts/src/views/home/HomeDesktop.ts
+++ exercises/01-basic-exercises/exercises-todo-list/todo-list-ts/src/views/home/HomeDesktop.ts
@@ -3,7 +3,8 @@ import { LitElement, html, css } from 'lit'
 import { customElement } from 'lit/decorators.js'
 
 // Layout
-import '@layouts/MainLayout'
+// import '@layouts/MainLayout'
+import '../../layouts/MainLayout'
 
 @customElement('home-desktop')
 export class HomeDesktop extends LitElement {
@@ -11,11 +12,11 @@ export class HomeDesktop extends LitElement {
   render() {
     return html`
 
-            <main-layout-desktop>
+            <main-layout>
                 <div>
                     <h4>Home Layout Desktop</h4>
                 </div>
-            </main-layout-desktop>
+            </main-layout>
 
         `
   }
