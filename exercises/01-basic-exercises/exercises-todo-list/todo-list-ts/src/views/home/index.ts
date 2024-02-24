dcdf2d5900169a51e0ff62bf10e5327b9bd5e622
Andres Giraldo <andresganc@gmail.com>
2024-02-24 18:36:43 -0500

    Mod: Exercises: Todo List TS: Index Home

---
+++ exercises/01-basic-exercises/exercises-todo-list/todo-list-ts/src/views/home/index.ts
@@ -0,0 +1,8 @@
+
+import { LitElement, html, css } from 'lit'
+import { customElement }  from 'lit/decorators.js'
+
+@customElement('index-home')
+export class IndexHome extends LitElement {
+    
+}
