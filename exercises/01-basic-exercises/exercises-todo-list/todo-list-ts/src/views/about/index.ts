73ce33f26b571dfcf6f27bb91b18362e632e53a4
Andres Giraldo <andresganc@gmail.com>
2024-02-25 09:33:04 -0500

    Mod: Exercises: Todo List: Index Components

---
+++ exercises/01-basic-exercises/exercises-todo-list/todo-list-ts/src/views/about/index.ts
@@ -0,0 +1,26 @@
+
+import { LitElement, html } from 'lit'
+import { customElement } from 'lit/decorators.js'
+
+// Components
+import './AboutMobile'
+import './AboutDesktop'
+
+@customElement('index-about')
+export class IndexAbout extends LitElement {
+
+  render() {
+
+    if (window.screen.width <= 425) {
+      return html`<about-mobile></about-mobile>`
+    }
+
+    if (window.screen.width > 425) {
+      return html`<about-desktop></about-desktop>`
+    }
+
+    return html`Error al cargar el componente`
+
+  }
+
+}
--- exercises/01-basic-exercises/exercises-todo-list/todo-list-ts/src/views/home/index.ts
+++ exercises/01-basic-exercises/exercises-todo-list/todo-list-ts/src/views/home/index.ts
@@ -1,8 +1,24 @@
 
-import { LitElement, html, css } from 'lit'
-import { customElement }  from 'lit/decorators.js'
+import { LitElement, html } from 'lit'
+import { customElement } from 'lit/decorators.js'
+
+// Components
+import './HomeMobile'
+import './HomeDesktop'
 
 @customElement('index-home')
 export class IndexHome extends LitElement {
-    
+
+  render() {
+
+    if (window.screen.width <= 425) {
+      return html`<home-mobile></home-mobile>`
+    }
+
+    if (window.screen.width > 425) {
+      return html`<home-desktop></home-desktop>`
+    }
+
+  }
+
 }
