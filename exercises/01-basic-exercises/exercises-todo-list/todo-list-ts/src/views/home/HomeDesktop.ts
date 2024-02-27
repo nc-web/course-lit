82bb06d5db0ca3e1c40e49fec478b03a3ae969ec
Andres Giraldo <andresganc@gmail.com>
2024-02-27 12:55:49 -0500

    Mod: Exercises Todo List: Home

--- exercises/01-basic-exercises/exercises-todo-list/todo-list-ts/src/views/home/HomeDesktop.ts
+++ exercises/01-basic-exercises/exercises-todo-list/todo-list-ts/src/views/home/HomeDesktop.ts
@@ -6,9 +6,10 @@ import { customElement } from 'lit/decorators.js'
 // import '@/layouts/MainLayoutDesktop'
 import '../../layouts/MainLayoutDesktop'
 
-// Cards
-import '../todo-list/cards/Card01'
-import '../todo-list/cards/Card02'
+// Images
+const Logo = new URL('../../../public/images/nc-web-logo.png', import.meta.url).href
+const Lit = new URL('../../../public/images/lit.svg', import.meta.url).href
+const TS = new URL('../../../public/images/typescript-01.png', import.meta.url).href
 
 @customElement('home-desktop')
 export class HomeDesktop extends LitElement {
@@ -16,35 +17,47 @@ export class HomeDesktop extends LitElement {
   render() {
     return html`
             <main-layout-desktop>
-
-                <div class='header__images'>
-
-                </div>
-
-                <div class='header__title'>
-                    <h2>TODO LIST'S - TYPESCRIPT</h2>
-                </div>
-
-                <div class='header__body'>
-                    <div class='header__body_cont_autor'>
-                        <h3 class='header__body_cont_autor_title'>Autor</h3>
-                        <p class='header__body_cont_autor_autor'>Andres Giraldo</p>
+                <header class='header'>
+                    <div>
+                        <img src='${Logo}' width='120px'/>
+                    </div>
+                    <div class='header__images'>
+                        <img class='header__images_lit' src='${Lit}'/>
+                        <img class='header__images_ts' src='${TS}'/>
                     </div>
 
-                    <div class='header__body_cont_tdetails'>
-                        <h3>Detalles Tecnicos</h3>
+                    <div class='header__title'>
+                        <h2>TODO LIST'S - TYPESCRIPT</h2>
+                        <small class='header__body_cont_autor_autor'>NC Todo List TS es un entorno para practicar los web components con lit y typescrip</small>
+                        <small class='header__body_cont_autor_autor'>Puede realizar un pu</small>
                     </div>
 
-                    <div class='header__body_cont_tdetails'>
-                        <h4>Todo List</h4>
-                        <small>Todo list basico hecho con Lit, Typescript, CSS Nativo Modular y Vistas responsivas. Solo con la opcion de agregar.</small>
+                    <div class='header__body'>
+                        <div class='header__body_cont_autor'>
+                            <h3 class='header__body_cont_autor_title'>Requisitos</h3>
+                            <p class='header__body_cont_autor_autor'>Los ejercicios se deben realizar con Lit, Typescript y Estilos con CSS Nativo Modular</p>
+                        </div>
+
+                        <div class='header__body_cont_tdetails'>
+                            <h3>Detalles tecnicos del entorno</h3>
+                        </div>
+
+                        <div class='header__body_cont_tdetails'>
+                            <h4>Estilos</h4>
+                            <small>CSS Module</small>
+                        </div>
+
+                        <div class='header__body_cont_tdetails'>
+                            <h4>Router</h4>
+                            <small>Libreria Lit Labs Router.</small>
+                        </div>
+                        
+                        <div class='header__body_cont_tdetails'>
+                            <h4>Estados Globales</h4>
+                            <small>Libreria Lit Context, Usado en los temas</small>
+                        </div>
                     </div>
-                    
-                    <div class='header__body_cont_tdetails'>
-                        <h4>Entorno</h4>
-                        <small>El entorno tiene un enrutamiento con Lit Labs Router, Temas administrados con Lit Context y CSS Module. </small>
-                    </div>
-                </div>
+                </header>
             </main-layout-desktop>
         `
   }
@@ -65,7 +78,16 @@ export class HomeDesktop extends LitElement {
             justify-content: center;
             align-items: center;
             text-align: center;
+            margin: 2rem 0;
             padding: 0 2rem;
+        }
+        .header__images_lit {
+            width: 3rem;
+            margin-right: .5rem;
+        }
+        .header__images_ts {
+            width: 3rem;
+            margin-left: .5rem;
         }
         .header__title {
             color: #FFAB00;
@@ -91,7 +113,7 @@ export class HomeDesktop extends LitElement {
             color: #2962FF;
         }
         .header__body_cont_tdetails {
-            margin: 1rem 0;
+            margin: 2rem 0 3rem 0;
             color: #2962FF;
         }
 
