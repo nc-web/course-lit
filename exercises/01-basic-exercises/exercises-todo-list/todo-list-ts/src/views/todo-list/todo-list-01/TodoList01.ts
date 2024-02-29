621b2e75c88ce7a6ef1c90385662f3c8c8bbd365
Andres Giraldo <andresganc@gmail.com>
2024-02-28 22:21:05 -0500

    Mod: Exercises Todo List TS: Check Ok

--- exercises/01-basic-exercises/exercises-todo-list/todo-list-ts/src/views/todo-list/todo-list-01/TodoList01.ts
+++ exercises/01-basic-exercises/exercises-todo-list/todo-list-ts/src/views/todo-list/todo-list-01/TodoList01.ts
@@ -49,7 +49,12 @@ export class TodoList01 extends LitElement {
                     <div class='tla__task_items'>
                       
                       <div class='tla__task_items_task'>
-                        <svg class='tla__taks_items_task_point' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>star-four-points-circle</title><path d="M8.17 2.76C9.39 2.26 10.69 2 12 2C13.31 2 14.61 2.26 15.83 2.76C17.04 3.26 18.14 4 19.07 4.93C20 5.86 20.74 6.96 21.24 8.17C21.74 9.39 22 10.69 22 12C22 14.65 20.95 17.2 19.07 19.07C17.2 20.95 14.65 22 12 22C10.69 22 9.39 21.74 8.17 21.24C6.96 20.74 5.86 20 4.93 19.07C3.05 17.2 2 14.65 2 12C2 9.35 3.05 6.8 4.93 4.93C5.86 4 6.96 3.26 8.17 2.76M12 17L13.56 13.58L17 12L13.56 10.44L12 7L10.43 10.44L7 12L10.43 13.58L12 17Z" /></svg>
+                        ${
+  x.status === true
+    ? html`<svg class='tla__taks_items_task_point_true' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>check-decagram</title><path d="M23,12L20.56,9.22L20.9,5.54L17.29,4.72L15.4,1.54L12,3L8.6,1.54L6.71,4.72L3.1,5.53L3.44,9.21L1,12L3.44,14.78L3.1,18.47L6.71,19.29L8.6,22.47L12,21L15.4,22.46L17.29,19.28L20.9,18.46L20.56,14.78L23,12M10,17L6,13L7.41,11.59L10,14.17L16.59,7.58L18,9L10,17Z" /></svg>`
+    : html`<svg class='tla__taks_items_task_point_false' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>star-four-points-circle</title><path d="M8.17 2.76C9.39 2.26 10.69 2 12 2C13.31 2 14.61 2.26 15.83 2.76C17.04 3.26 18.14 4 19.07 4.93C20 5.86 20.74 6.96 21.24 8.17C21.74 9.39 22 10.69 22 12C22 14.65 20.95 17.2 19.07 19.07C17.2 20.95 14.65 22 12 22C10.69 22 9.39 21.74 8.17 21.24C6.96 20.74 5.86 20 4.93 19.07C3.05 17.2 2 14.65 2 12C2 9.35 3.05 6.8 4.93 4.93C5.86 4 6.96 3.26 8.17 2.76M12 17L13.56 13.58L17 12L13.56 10.44L12 7L10.43 10.44L7 12L10.43 13.58L12 17Z" /></svg>`
+}
+                        
                         <p class='tla__taks_items_task_text' >${x.id} - ${x.task} - ${x.status}</p>
                       </div>
                       
@@ -109,7 +114,22 @@ export class TodoList01 extends LitElement {
   }
 
   checkTask(id: number) {
-    console.log('Check task', id)
+
+    const taskFound = this.stateTasks.find(x => x.id === id)
+
+    if (taskFound) {
+
+      if (taskFound.status === false) {
+        taskFound.status = true
+        this.requestUpdate()
+      } else {
+        taskFound.status = false
+        this.requestUpdate()
+      }
+    }
+
+    // console.log('Check task', id)
+    // console.log('Check task', taskFound)
   }
 
 
@@ -174,9 +194,14 @@ export class TodoList01 extends LitElement {
       justify-content: flex-start;
       align-items: center;
     }
-    .tla__taks_items_task_point{
+    .tla__taks_items_task_point_false{
       width: 1.3rem;
       fill: #9E9E9E;
+      margin-right: .3rem;
+    }
+    .tla__taks_items_task_point_true{
+      width: 1.3rem;
+      fill: #64DD17;
       margin-right: .3rem;
     }
 
