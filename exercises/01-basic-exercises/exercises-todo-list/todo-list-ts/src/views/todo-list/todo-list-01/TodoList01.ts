e427e0f9a24bca510373ced88a96ffdf59219b18
Andres Giraldo <andresganc@gmail.com>
2024-04-30 12:05:10 -0500

    Mod: Exercises: Todo List TS: Todo List 01 99%

--- exercises/01-basic-exercises/exercises-todo-list/todo-list-ts/src/views/todo-list/todo-list-01/TodoList01.ts
+++ exercises/01-basic-exercises/exercises-todo-list/todo-list-ts/src/views/todo-list/todo-list-01/TodoList01.ts
@@ -39,8 +39,7 @@ export class TodoList01 extends LitElement {
   // VIEWS
   render() {
     return html`
-            <section class='tla'>
-                
+            <section class='tla'> 
                 <!-- Header -->
                 <div class='tla__header'>
                   <h2 class='tla__header_title'>TODO LIST BASIC - TS</h2>
@@ -54,25 +53,23 @@ export class TodoList01 extends LitElement {
 
                 <!-- Tasks -->
                 <div class='tla__tasks'>
-                  ${this.stateTasks.map(x => html`
+                  ${this.stateTasks.map((x, index) => html`
                     <div class='tla__tasks_items'>
                       
                       <div class='tla__tasks_items_tasks'>
                         <svg class='tla__tasks_items_tasks_point_false' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>star-four-points-circle</title><path d="M8.17 2.76C9.39 2.26 10.69 2 12 2C13.31 2 14.61 2.26 15.83 2.76C17.04 3.26 18.14 4 19.07 4.93C20 5.86 20.74 6.96 21.24 8.17C21.74 9.39 22 10.69 22 12C22 14.65 20.95 17.2 19.07 19.07C17.2 20.95 14.65 22 12 22C10.69 22 9.39 21.74 8.17 21.24C6.96 20.74 5.86 20 4.93 19.07C3.05 17.2 2 14.65 2 12C2 9.35 3.05 6.8 4.93 4.93C5.86 4 6.96 3.26 8.17 2.76M12 17L13.56 13.58L17 12L13.56 10.44L12 7L10.43 10.44L7 12L10.43 13.58L12 17Z" /></svg>
-                        <p class='tla__taskss_items_tasks_text'>${x}</p>
+                        <p class='tla__taskss_items_tasks_text'> ${index} - ${x}</p>
                       </div>
                       
                       <div class='tla__tasks_items_act'>
-                        <a class='tla__tasks_items_act_a' @click=${this.removeTask}>
+                        <a class='tla__tasks_items_act_a' @click=${() => this.removeTask(index)}>
                           <svg class='tla__tasks_items_act_delete' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>delete</title><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>
                         </a>
                       </div>
 
                     </div>  
-
                   `)}
                 </div>
-
             </section>
         `
   }
@@ -92,15 +89,16 @@ export class TodoList01 extends LitElement {
   addTask() {
     this.stateTasks.push(this.propInputTask)
     this.queryInputTask.value = ''
+    this.requestUpdate()
     console.log(this.stateTasks)
   }
 
-  removeTask(id: number) {
-    // this.stateTasks = [...this.stateTasks.filter(x => x.id !== id)]
-    // this.requestUpdate()
-    console.log('Task removed', id)
+  removeTask(index: number) {
+    this.stateTasks = [...this.stateTasks.filter((x, i) => i !== index)]
+    this.requestUpdate()
+    console.log(this.stateTasks)
+    console.log('Task removed', index)
   }
-
 
 
   // STYLES
