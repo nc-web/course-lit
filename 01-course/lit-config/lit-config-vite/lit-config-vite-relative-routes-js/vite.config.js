ad2409185b7b6e5772fce8251f7580f997473239
Andres Giraldo <andresganc@gmail.com>
2026-02-19 16:40:04 -0500

    MOD: Course Lit config vite: Added vite.config.js

---
+++ 01-course/lit-config/lit-config-vite/lit-config-vite-relative-routes-js/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@templates': path.resolve(__dirname, './src/templates'),
+      '@views': path.resolve(__dirname, './src/views'),
+      
+    },
+  },
+});
